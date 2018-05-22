d2573ec9b27ce4b179bfaa075f3ded95baf167bb
Web Developer <smilemonkey1230@outlook.com>
2018-05-23 01:16:40 +0700

    change mid menu style and other styles

--- wp-content/themes/uncode-child/assets/js/main.js
+++ wp-content/themes/uncode-child/assets/js/main.js
@@ -13,4 +13,15 @@ jQuery(document).ready(function($) {
 	}
 	isTouchSupported();
 	$('.new-badge img').before('<img class="new-icon"/>');
+	$('.mid-menu .mid-menu-logo').after('<div class="uncode_text_column mid-menu-drop"><p><i class="fa fa-angle-down"></i><i class="fa fa-angle-up"></i></p></div>');
+	$('.mid-menu .mid-menu-drop p .fa').on('click', function(){
+		if ($('.mid-menu-content .menu-smart').css('display') == 'none') {
+			$('.mid-menu .mid-menu-drop p .fa.fa-angle-down').hide();
+			$('.mid-menu .mid-menu-drop p .fa.fa-angle-up').show();
+		} else {
+			$('.mid-menu .mid-menu-drop p .fa.fa-angle-down').show();
+			$('.mid-menu .mid-menu-drop p .fa.fa-angle-up').hide();
+		}
+		$('.mid-menu-content .menu-smart').toggle("slow");
+	});
 });
